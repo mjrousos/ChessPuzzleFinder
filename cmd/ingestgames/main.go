049a58a2e2e94988ec51d193a2b0d8a137174bfa
Mike Rousos <Mike@MJRousos.com>
2020-05-21 18:09:34 -0400

    Add configuration

--- cmd/ingestgames/main.go
+++ cmd/ingestgames/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/mjrousos/ChessPuzzleFinder/tactics"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	showHeader()
 	configure()
 
+	queueName := viper.GetString("GameIngestionQueue")
+	fmt.Printf("Processing games from queue %v\n", queueName)
 	puzzles := tactics.FindPuzzles(nil)
 	fmt.Printf("Identified %v puzzles\n", len(puzzles))
 
@@ -25,4 +30,17 @@ func showHeader() {
 
 func configure() {
 	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AutomaticEnv()
+
+	// Look for config next to the exe and in the current working dir
+	appPath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	viper.AddConfigPath(appPath)
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Could not read config file")
+		panic(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mjrousos/ChessPuzzleFinder
 
 go 1.14
 
-require github.com/pkg/errors v0.9.1
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/spf13/viper v1.7.0
+)
